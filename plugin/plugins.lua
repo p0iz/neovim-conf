abf80c254d64ab70aee892cd4ee5031ef26bc0b9
Tommi Martela <tommi.martela@gmail.com>
2023-02-02 21:45:07 +0200

    Use tabs in all files

--- plugin/plugins.lua
+++ plugin/plugins.lua
@@ -1,32 +1,32 @@
 local bootstrap_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
 
 local bootstrapped = bootstrap_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-  -- Color scheme
-  use {
+	-- Color scheme
+	use {
 		'artanikin/vim-synthwave84',
 		config = 'vim.cmd[[colorscheme synthwave84]]'
-  }
+	}
 
 	use 'sainnhe/gruvbox-material'
 	use	'folke/tokyonight.nvim'
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if bootstrapped then
-    require('packer').sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if bootstrapped then
+		require('packer').sync()
+	end
 end)
 
