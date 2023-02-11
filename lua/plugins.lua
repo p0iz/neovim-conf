41116bd9b1860648b2ae661d56ef1ccf3ddda1e1
Tommi Martela <tommi.martela@gmail.com>
2023-02-11 15:26:30 +0200

    Add Telescope.nvim and nvim-tree bindings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,7 +8,41 @@ return {
 	-- File browsing
 	{
 		'nvim-tree/nvim-tree.lua',
-		dependencies = { 'nvim-tree/nvim-web-devicons' } 
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require("nvim-tree").setup({
+				sort_by = "case_insensitive",
+				view = {
+					width = 40,
+					mappings = {
+						list = {
+							{ key = "u", action = "dir_up" },
+							{ key = ".", action = "cd" },
+						},
+					},
+				},
+				renderer = {
+					group_empty = true,
+				},
+				filters = {
+					dotfiles = true,
+				},
+			})
+			local api = require("nvim-tree.api")
+			vim.keymap.set('n', '<leader>ft', api.tree.toggle, { silent = true })
+		end
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		-- tag = '0.1.1',
+	  requires = { 'nvim-lua/plenary.nvim'},
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		end
 	},
 	-- LSP
 	{
