3d359ef90f91b411907d4e48e9091d14a7a9cb5b
Tommi Martela <tommi.martela@gmail.com>
2023-02-27 14:21:55 +0200

    Add default rust-tools

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -66,6 +66,26 @@ return {
 			vim.keymap.set('n', 'gd', require('omnisharp_extended').lsp_definitions)
 		end
 	},
+	-- Rust
+	{
+		'simrat39/rust-tools.nvim',
+		dependencies = {
+			'neovim/nvim-lspconfig'
+		},
+		config = function()
+			local rt = require("rust-tools") 
+			rt.setup({
+				server = {
+					on_attach = function(_, bufnr)
+						-- Hover actions
+						vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+						-- Code action groups
+						vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+					end,
+				},
+			})
+		end
+	},
 
 	-- Color schemes
 	{
